74d81c950cd3bd35c0455d7d677c97bd34b96884
Gye11 <hilallornekk@gmail.com>
2026-03-01 02:45:45 +0300

    final

---
+++ src/options/options.jsx
@@ -0,0 +1,33 @@
+import styles from "./Options.module.css";
+
+function Options({ updateFeedback, resetFeedback, totalFeedback }) {
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.button} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+
+      <button
+        className={styles.button}
+        onClick={() => updateFeedback("neutral")}
+      >
+        Neutral
+      </button>
+
+      <button className={styles.button} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+
+      {totalFeedback > 0 && (
+        <button
+          className={`${styles.button} ${styles.reset}`}
+          onClick={resetFeedback}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Options;
